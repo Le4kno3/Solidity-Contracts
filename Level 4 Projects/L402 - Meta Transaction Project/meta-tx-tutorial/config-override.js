94b49deba75192a48901b870102da4156287262d
le4kno3 <le4kno3@gmail.com>
2023-01-27 18:15:14 +0530

    updates in projects

---
+++ Level 4 Projects/L402 - Meta Transaction Project/meta-tx-tutorial/config-override.js
@@ -0,0 +1,43 @@
+const { alias } = require('react-app-rewire-alias');
+const path = require(`path`);
+const webpack = require("webpack");
+
+module.exports = function override(config) {
+    const fallback = config.resolve.fallback || {};
+    Object.assign(fallback, {
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
+        assert: require.resolve("assert"),
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        os: require.resolve("os-browserify"),
+        url: require.resolve("url"),
+        path: require.resolve("path-browserify")
+    });
+    config.resolve.fallback = fallback;
+    config.plugins = (config.plugins || []).concat([
+        new webpack.ProvidePlugin({
+            process: "process/browser",
+            Buffer: ["buffer", "Buffer"],
+        }),
+    ]);
+    config.module.rules.unshift({
+        test: /\.m?js$/,
+        resolve: {
+            fullySpecified: false, // disable the behavior
+        },
+    });
+    config.ignoreWarnings = [/Failed to parse source map/];
+
+    alias({
+        "@components": path.resolve(__dirname, "src/components/"),
+        "@cache": path.resolve(__dirname, "src/cache"),
+        "@libraries": path.resolve(__dirname, "src/libraries"),
+        "@datatypes": path.resolve(__dirname, "src/types"),
+        "@config": path.resolve(__dirname, "src/config.ts"),
+        "@api": path.resolve(__dirname, "src/api"),
+        "@assets": path.resolve(__dirname, "src/assets"),
+        "@artifacts": path.resolve(__dirname, "src/artifacts"),
+    })(config);
+    return config;
+};
